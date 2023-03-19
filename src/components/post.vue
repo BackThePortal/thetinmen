5655033e2be88939eaf45edede5eab2fc7dcc354
BackThePortal <backtheportal@gmail.com>
2023-03-19 14:39:31 +0100

    post.vue changes

--- src/components/post.vue
+++ src/components/post.vue
@@ -5,13 +5,13 @@
 		<div>
 			<img :alt="data?.alt ?? data.title" ref="image" :src="getImagePath(data.source)" class="select-none z-0 transition duration-500 rounded-lg w-32 h-40 sm:w-48 sm:h-60 md:w-64 md:h-80 xl:w-96 xl:h-120 shadow-md  group-hover:brightness-50 contrast-more:group-hover:brightness-[.15] group-hover:blur-md group-hover:saturate-50">
 			<div class="transform-gpu subpixel-antialiased transition-all duration-500 absolute bottom-1/2 font-semibold px-3 w-32 sm:w-48 md:w-64 xl:w-96 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl z-50 text-slate-200 opacity-0 sm:group-hover:opacity-80">
-				<span class="font-light">{{objectToDate(data.date)}}</span>
+				<span v-if="data.date" class="font-light">{{ objectToDate(data.date) }}</span>
 				<br>
 				<span>{{data.title}}</span>
 			</div>
 			<div class="transform-gpu subpixel-antialiased transition-all duration-500 absolute bottom-1/2 text-sm sm:text-xs md:text-sm lg:text-base sm:translate-y-16 w-32 sm:w-48 md:w-64 xl:w-96 text-center z-50 opacity-0 group-hover:opacity-80 flex flex-col items-center align-top content-start gap-1">
-				<a :href="data.link" target="_blank" class="transition-all text-slate-200 hover:font-semibold underline underline-offset-2  hover:decoration-2 hover:bg-slate-300/30 active:bg-slate-300/60 pt-0-5 pb-1 px-2 rounded w-fit">See post</a>
-				<a v-if="data.blog && data.blog !== ''" :href="data.blog" target="_blank" class="transition-all text-slate-200 hover:font-semibold underline underline-offset-2  hover:decoration-2 hover:bg-slate-300/30 active:bg-slate-300/60 pt-0-5 pb-1 px-2 rounded w-fit">See blog entry</a>
+				<a v-if="data.link" :href="data.link" target="_blank" class="link">See post</a>
+				<a v-if="data.blog" :href="data.blog" target="_blank" class="link">See blog entry</a>
 			</div>
 		</div>
 
@@ -20,12 +20,14 @@
 
 <script setup>
 import { getImagePath, objectToDate, prettifyURL } from "@/js/utils";
-import {ref} from "vue";
+import { ref } from "vue";
 
 const props = defineProps(['data', 'id']);
 const link = ref(prettifyURL(props.data.link))
 </script>
 
 <style scoped>
-
+.link {
+	@apply transition-all text-slate-200 hover:font-semibold underline underline-offset-2 hover:decoration-2 hover:bg-slate-300/30 active:bg-slate-300/60  pb-1 px-2 rounded w-fit
+}
 </style>

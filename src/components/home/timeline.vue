22cc8de54f07b89eba8907c503b432c51f8b5277
BackThePortal <backtheportal@gmail.com>
2023-03-31 15:47:18 +0200

    Improved "in construction" page in Timeline

--- src/components/home/timeline.vue
+++ src/components/home/timeline.vue
@@ -1,5 +1,13 @@
 <template>
-<div>
-<span>This feature is being developed.</span>
-</div>
+	<div>
+		<p class="text-slate-700 dark:text-slate-300">
+			This feature is being developed.<br />Do you want to collaborate? Check
+			out the
+			<a
+				class="text-slate-900 dark:text-slate-200 hover:cursor-pointer hover:underline underline-offset-2"
+				href="https://github.com/BackThePortal/thetinmen"
+				>GitHub repo</a
+			>.
+		</p>
+	</div>
 </template>

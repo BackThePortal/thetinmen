a871c8fa965291e2833015b6fdfccfabe56fbeb5
BackThePortal <backtheportal@gmail.com>
2023-04-20 17:30:10 +0200

    Reorganized Vue components

---
+++ src/components/home/timeline/Index.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<p class="text-black dark:text-slate-200">
+			This feature is being developed.<br />Do you want to collaborate? Check
+			out the
+			<a class="link" href="https://github.com/BackThePortal/thetinmen"
+				>GitHub repo</a
+			>.
+		</p>
+	</div>
+</template>

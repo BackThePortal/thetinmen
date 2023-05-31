8a2284be39fa219ac11c55ce317b428a8899f5d1
BackThePortal <backtheportal@gmail.com>
2023-05-31 21:27:40 +0200

    Improved copyright info

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,13 @@
 				<component :is="Component" />
 			</transition>
 		</router-view>
-		<p
-			class="mt-2 text-sm text-center mx-2 text-slate-700 dark:text-slate-300 font-normal"
-		>
+		<p>
 			The creators of this website are not affiliated with the administrator of
 			the Instagram account. This project has been created with their
 			permission.
 		</p>
+		<p>Posts: All rights reserved. &copy;2023 TheTinMen</p>
+		<p>Code: Some rights reserved. MIT License &copy;2023 BackThePortal</p>
 	</div>
 </template>
 
@@ -30,5 +30,9 @@ import NavBar from '!/home/NavBar.vue';
 .fade-enter-from,
 .fade-leave-to {
 	opacity: 0;
+}
+
+p {
+	@apply mt-2 text-sm text-center mx-2 text-slate-700 dark:text-slate-300 font-normal;
 }
 </style>

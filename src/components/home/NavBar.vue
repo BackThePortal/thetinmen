53b6efd7840944d945d4b673228dd8f19ba04dc0
BackThePortal <backtheportal@gmail.com>
2023-08-18 12:47:15 +0200

    Improved NavBar look

--- src/components/home/NavBar.vue
+++ src/components/home/NavBar.vue
@@ -1,27 +1,27 @@
 <template>
 	<div
-		class="transition-all xs:sticky xs:top-0 xs:left-0 xs:right-0 z-50"
+		class="z-50 transition-all xs:sticky xs:top-0 xs:left-0 xs:right-0"
 		:class="[scroll > scrollGoal ? 'p-0' : 'sm:px-5']"
 	>
 		<header
-			class="transition-all bg-slate-300/40 dark:bg-slate-500/40 contrast-more:bg-slate-400 dark:contrast-more:bg-slate-700 contrast-more:backdrop-blur-0 backdrop-blur-lg p-4 text-center z-50 shadow-lg"
+			class="z-50 bg-slate-300/40 p-4 text-center shadow-lg backdrop-blur-lg transition-all contrast-more:bg-slate-400/80 contrast-more:backdrop-blur-xl dark:bg-slate-500/40 dark:contrast-more:!bg-slate-700/80"
 			:class="[
 				scroll > scrollGoal
-					? 'xs:bg-slate-200/60 xs:dark:bg-slate-600/40 m-0 xs:shadow-2xl'
-					: 'max-w-7xl mx-auto sm:rounded-b-xl',
+					? 'm-0 xs:bg-slate-200/60 xs:shadow-2xl xs:dark:bg-slate-600/40'
+					: 'mx-auto max-w-7xl sm:rounded-b-xl',
 			]"
 		>
-			<div class="flex items-center mb-2 justify-center">
+			<div class="mb-2 flex items-center justify-center">
 				<div
-					class="transition px-4 py-1 bg-slate-700/60 dark:bg-gray-800/60 hover:bg-slate-600/50 dark:hover:bg-slate-800/20 rounded-lg active:opacity-40"
+					class="rounded-lg bg-slate-700/60 px-4 py-1 transition hover:bg-slate-600/50 active:opacity-40 dark:bg-gray-800/60 dark:hover:bg-slate-800/20"
 					:class="[
 						scroll > scrollGoal
-							? 'bg-slate-700/0 dark:bg-slate-gray-800/0'
+							? 'dark:bg-slate-gray-800/0 bg-slate-700/0'
 							: '',
 					]"
 				>
 					<h1
-						class="select-none bg-clip-text text-transparent bg-gradient-to-r"
+						class="select-none bg-gradient-to-r bg-clip-text text-transparent"
 						:class="[
 							backo
 								? 'from-sky-500 to-teal-600'
@@ -35,18 +35,20 @@
 			</div>
 
 			<nav
-				class="mt-2 sm:mt-4 flex flex-initial max-xs:flex-col-reverse max-xs:gap-2 xs:justify-around items-center place-content-center"
+				class="mt-2 flex flex-initial place-content-center items-center max-xs:flex-col-reverse max-xs:gap-2 xs:justify-around sm:mt-4"
 			>
-				<span class="text-black dark:text-slate-200 basis-1/3">
-					<b>{{ counts.posts }}</b> post{{ counts.posts > 1 ? 's' : ''
+				<span class="basis-1/3 text-slate-800 dark:text-slate-200">
+					<b class="text-black dark:text-slate-200">{{ counts.posts }}</b>
+					post{{ counts.posts > 1 ? 's' : ''
 					}}<span
 						class="before:content-[',_'] sm:before:content-['_in_']"
 					></span>
-					<b>{{ counts.topics }}</b> topic{{ counts.topics > 1 ? 's' : '' }}
+					<b class="text-black dark:text-slate-200">{{ counts.topics }}</b>
+					topic{{ counts.topics > 1 ? 's' : '' }}
 				</span>
 
 				<span
-					class="text-black dark:text-slate-200 max-md:hidden basis-1/3 min-w-fit"
+					class="min-w-fit basis-1/3 text-slate-800 dark:text-slate-200 max-md:hidden"
 					>Visit
 					<a
 						target="_blank"
@@ -59,7 +61,7 @@
 
 				<!-- md -->
 				<span
-					class="text-black dark:text-slate-200 max-sm:hidden md:hidden basis-1/3"
+					class="basis-1/3 text-slate-800 dark:text-slate-200 max-sm:hidden md:hidden"
 					>Visit
 					<a target="_blank" class="link" href="https://instagram.com/thetinmen"
 						>@thetinmen</a
@@ -68,11 +70,11 @@
 				>
 
 				<!-- xs -->
-				<span class="sm:hidden basis-1/3">
+				<span class="basis-1/3 sm:hidden">
 					<a class="link" href="https://instagram.com/thetinmen">@thetinmen</a>
 				</span>
 
-				<span class="text-black dark:text-slate-200 basis-1/3"
+				<span class="basis-1/3 text-slate-800 dark:text-slate-200"
 					>Theme:
 					<button @click="handleThemeSwitch" class="action-link">
 						{{ themeButtonLabel }}
